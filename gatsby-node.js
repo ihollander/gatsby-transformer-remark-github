f48667cc41e6878a5048609c662d524bd07ad270
ihollander <ianholla@gmail.com>
2019-06-30 10:13:53 -0400

    initial commit;

---
+++ gatsby-node.js
@@ -0,0 +1,34 @@
+const crypto = require("crypto");
+const digest = str =>
+  crypto
+    .createHash("md5")
+    .update(str)
+    .digest("hex");
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNode, createNodeField } = actions;
+
+  if (node.context && node.context.repository) {
+    if (node.context.repository.readme) {
+      const textNode = {
+        id: `${node.id}-MarkdownBody`,
+        parent: node.id,
+        internal: {
+          type: `MarkdownRemark`,
+          mediaType: "text/markdown",
+          content: node.context.repository.readme.text,
+          contentDigest: digest(node.context.repository.readme.text)
+        }
+      };
+
+      createNode(textNode);
+
+      createNodeField({
+        node,
+        name: "markdownBody___NODE",
+        value: textNode.id
+      });
+    }
+  }
+  return;
+};
